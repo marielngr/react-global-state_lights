1c8836d720038800a4b6de0cd52b79e62b64a652
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-01-23 23:00:06 +0100

    change lights

--- pages/_app.js
+++ pages/_app.js
@@ -9,3 +9,27 @@ export default function App({ Component, pageProps }) {
     </Layout>
   );
 }
+
+/* hier Array:      <Light name="Living Room" />
+      </li>
+      <li>
+        <Light name="Kitchen" />
+      </li>
+      <li>
+        <Light name="Bedroom" />
+      </li>
+      <li>
+        <Light name="Bathroom" />
+      </li>
+      <li>
+        <Light name="Garage" />
+      </li>
+      <li>
+        <Light name="Porch" />
+      </li>
+      <li>
+        <Light name="Garden" />
+      </li>
+      <li>
+        <Light name="Office" />
+      </li>*/
