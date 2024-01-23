ba69a7911363d8d268a0a18b35993baa9365c6a3
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-01-23 22:35:32 +0100

    delete state

--- components/Light/index.js
+++ components/Light/index.js
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
-export default function Light({ name }) {
-  const [isOn, setIsOn] = useState(false);
-
+export default function Light({ name, isOn, onToggle }) {
   function handleToggle() {
-    setIsOn((isOn) => !isOn);
+    onToggle();
   }
 
   return (
